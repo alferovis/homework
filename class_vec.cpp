3f87c8e77909e69ecd6294b92b9912f9a19e17b7
ilya_alferov <alferov.is@phystech.edu>
2022-10-23 23:31:38 +0300

    added class square matrix

--- class_vec.cpp
+++ class_vec.cpp
@@ -28,12 +28,50 @@ void vector :: print(){
 	for (int i = 0; i < size; i++) {
 		cout << data[i] << " ";
 	}
+	cout << endl << endl;
+};
+
+void vector :: set_id(string id){
+	this->id = id;
+};
+
+
+class matrix{
+	int size;
+	string id;
+	double** data2d;
+public:
+	matrix(int size, string id);
+	void print();
+};
+
+matrix :: matrix(int size, string id = "unknown"){
+	this->size = size;
+	this->id = id;
+
+	data2d = new double* [size];
+	for (int i = 0; i <size; i++) {
+		data2d[i] = new double [size];
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			data2d[i][j] = (i+1)*(j+1);
+		}
+	}
+};
+
+void matrix :: print() {
+	cout << id << " " << size << endl;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << data2d[i][j] << " ";
+		}
+		cout << endl;
+	}
 	cout << endl;
 };
 
-void vecotr :: set_id(int size){
-	this->id = id;
-}
+
 
 int main(){
 	cout << "Hello, world" << endl;
@@ -43,6 +81,10 @@ int main(){
 	v1.print();
 	v2.print();
 
-	cout << v1.size;
+	//cout << v1.size;
+
+	matrix m(2, "first matrix");
+	m.print();
+
 	return 0;
 }
